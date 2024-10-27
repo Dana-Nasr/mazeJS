9eabba457d40085bed76ca4c3cf3e39232711389
dananasr2004 <dananasr871@gmail.com>
2024-10-27 22:11:38 +0200

    Add: stay within border

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,11 @@
 document.getElementById("start").addEventListener("mouseenter", startGame);
 const box = document.querySelector(".boundary.example");
+
 function startGame() {
   document.getElementById("start").style.display = "none";
   moveCursor();
+  const game = document.getElementById("game");
+  game.addEventListener("mouseleave",outOfGame);
 }
 
 function moveCursor() {
@@ -13,7 +16,7 @@ function moveCursor() {
 }
 
 function writeResult() {
-  document.getElementById("start").style.display = "block";
+  document.getElementById("end").style.display = "none";
   box.textContent = "game lost";
 }
 
@@ -21,4 +24,7 @@ document.getElementById("end").addEventListener("mouseenter", winGame);
 function winGame() {
   document.getElementById("end").style.display = "none";
   box.textContent = "game won";
+}
+function outOfGame() {
+  box.textContent = "game lost";
 }
