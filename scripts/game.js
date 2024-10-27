ff4e2409d086e4cc7160f0b505dd0792ab1e2cda
dananasr2004 <dananasr871@gmail.com>
2024-10-27 21:23:09 +0200

    Add: start game

---
+++ scripts/game.js
@@ -0,0 +1,8 @@
+document.getElementById("start").addEventListener("mouseenter", startGame);
+const box = document.querySelector(".boundary example");
+function startGame() {
+  document.getElementById("start").style.display = "none";
+  moveCursor();
+}
+
+
