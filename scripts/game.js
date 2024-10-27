bd0248abe2c58ac9d8c48f08e6ab08c981de9f26
dananasr2004 <dananasr871@gmail.com>
2024-10-27 21:25:24 +0200

    Add: barier check

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,19 @@
 document.getElementById("start").addEventListener("mouseenter", startGame);
-const box = document.querySelector(".boundary example");
+const box = document.querySelector(".boundary.example");
 function startGame() {
   document.getElementById("start").style.display = "none";
   moveCursor();
 }
 
+function moveCursor() {
+  const boundaries = document.querySelectorAll(".boundary");
+  boundaries.forEach((boundary) => {
+    boundary.addEventListener("mouseenter", writeResult);
+  });
+}
+
+function writeResult() {
+  document.getElementById("start").style.display = "block";
+  box.textContent = "game lost";
+}
 
